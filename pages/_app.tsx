4908b48874e6ca3b16cd21de47c6a9e4b3bd922b
nelv13 <jayhspark@gmail.com>
2021-02-04 17:12:39 +0900

    add graphql with mysql

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
-import '../styles/globals.css'
-import { AppProps } from 'next/dist/next-server/lib/router/router'
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
 
-
-
-function MyApp({ Component, pageProps }:AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
